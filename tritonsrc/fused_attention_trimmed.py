41583527cb7bf714956a35cd9cf5146ae201742d
Xinya Zhang <Xinya.Zhang@amd.com>
2023-12-07 22:26:37 +0000

    tritonsrc: remove unused python files

--- tritonsrc/fused_attention_trimmed.py
+++ tritonsrc/fused_attention_trimmed.py
@@ -16,5 +16,4 @@ import triton.language as tl
 
 from fwd_kernel import attn_fwd
 from bwd_preprocess import bwd_preprocess
-from bwd_kernel import bwd_kernel
 from bwd_split_kernel import bwd_kernel_dk_dv, bwd_kernel_dq
