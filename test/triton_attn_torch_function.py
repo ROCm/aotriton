c099e4db9959902ad31fc773aca803ace79e275c
Xinya Zhang <Xinya.Zhang@amd.com>
2024-01-22 18:13:02 +0000

    test: add triton reference tests for flash attention

--- test/triton_attn_torch_function.py
+++ test/triton_attn_torch_function.py
@@ -4,6 +4,8 @@ import torch
 import triton
 import triton.language as tl
 from fwd_kernel import attn_fwd as bare_attn_fwd
+from bwd_preprocess import bwd_preprocess
+from bwd_split_kernel import bwd_kernel_dk_dv, bwd_kernel_dq
 
 VERBOSE=False
 
@@ -178,6 +180,107 @@ class _attention(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, do, _, __):
-        pass
+        if torch.version.hip is not None:
+            BLOCK = 64
+        else:
+            BLOCK = 128
+        q, k, v, o, L = ctx.saved_tensors
+        # if q.shape[-1] <= 32:
+        do = do.contiguous()
+        dk = torch.empty_like(k)
+        dv = torch.empty_like(v)
+        delta = torch.empty_like(L)
+        do_scaled = torch.empty_like(do)
+        seqlen_q = q.shape[2]
+        seqlen_k = k.shape[2]
+        MAX_BLOCK = 64 if ctx.dropout_p == 0 else 16
+        # BLOCK = min(seqlen_q, seqlen_k, q.shape[-1], MAX_BLOCK)
+        BLOCK = 16 # FIXME: Variable block size
+        BLOCK = BLOCK if is_supported_by_tl_dot(seqlen_q) and is_supported_by_tl_dot(seqlen_k) else 1
+
+        # block size is (BLOCK_M, D_HEAD)
+        bwd_preprocess[(do.shape[0] * do.shape[1] * triton.cdiv(do.shape[2], BLOCK), )](
+            o, do,
+            do_scaled, delta,
+            BLOCK_M=BLOCK, D_HEAD=ctx.BLOCK_DMODEL,
+        )
+        if False or VERBOSE:
+            print(f'{q.shape=} {q.stride()=}')
+            print(f'{k.shape=} {k.stride()=}')
+            print(f'{v.shape=} {v.stride()=}')
+            print(f'{o.shape=} {o.stride()=}')
+            print(f'{dq.shape=} {dq.stride()=}')
+            print(f'{dk.shape=} {dk.stride()=}')
+            print(f'{dv.shape=} {dv.stride()=}')
+            print(f'{do.shape=} {do.stride()=}')
+            print(f'{L=} {L.shape=}')
+            print(f'{delta=}')
+            print(f'{BLOCK=}')
+        # debug_mask = torch.zeros((q.shape[0], q.shape[1], seqlen_q, seqlen_k), device=q.device, dtype=ctx.encoded_softmax.dtype)
+        bwd_kernel_dk_dv[(triton.cdiv(seqlen_k, BLOCK), ctx.grid[1])](
+            q, k, v, ctx.sm_scale,
+            o, do_scaled,
+            dk, dv,
+            L, delta,
+            q.stride(0), q.stride(1), q.stride(2), q.stride(3),
+            k.stride(0), k.stride(1), k.stride(2), k.stride(3),
+            v.stride(0), v.stride(1), v.stride(2), v.stride(3),
+            seqlen_q=seqlen_q,
+            seqlen_k=seqlen_k,
+            dropout_p=ctx.dropout_p,
+            philox_seed=ctx.philox_seed,
+            philox_offset_base=ctx.philox_offset,
+            # debug_mask=debug_mask,
+            BLOCK_M=BLOCK, BLOCK_N=BLOCK,
+            BLOCK_DMODEL=ctx.BLOCK_DMODEL,
+            CAUSAL=ctx.causal,
+            num_warps=4,
+            num_stages=1,
+            ENABLE_DROPOUT=ctx.dropout_p > 0.0,
+        )
+        # mask_allclose = torch.allclose(debug_mask < 0, ctx.encoded_softmax < 0)
+        if False:
+            mask_allclose = torch.allclose(torch.abs(debug_mask), torch.abs(ctx.encoded_softmax)) # Stores QK
+            if not mask_allclose:
+                torch.set_printoptions(linewidth=200, threshold=2000)
+                import sys
+                print(f'bwd mask: {torch.abs(debug_mask[:,:,:2,16:])}')
+                print(f'fwd mask: {torch.abs(ctx.encoded_softmax[:,:,:2,16:])}')
+                print(f'Full bwd mask: {debug_mask[0,0]}')
+                print(f'Full fwd mask: {ctx.encoded_softmax[0,0]}')
+                print(f'Full mask div: {debug_mask[0,0] / ctx.encoded_softmax[0,0]}')
+                print(f'Full dv: {dv}')
+                if seqlen_q == 32:
+                    print(f'2nd block bwd mask: {debug_mask[0,0, 16:]}')
+                    print(f'2nd block fwd mask: {ctx.encoded_softmax[0,0, 16:]}')
+            # print(f'Full q: {q}', file=sys.stderr)
+            # assert mask_allclose
+        if q.requires_grad:
+            dq = torch.zeros_like(q, dtype=torch.float32)
+            DQ_BLOCK_M = min(seqlen_q, BLOCK)
+            bwd_kernel_dq[(triton.cdiv(q.shape[2], DQ_BLOCK_M), q.shape[0] * q.shape[1])](
+                q, k, v, ctx.sm_scale,
+                o, do_scaled,
+                dq,
+                L, delta,
+                q.stride(0), q.stride(1), q.stride(2), q.stride(3),
+                k.stride(0), k.stride(1), k.stride(2), k.stride(3),
+                v.stride(0), v.stride(1), v.stride(2), v.stride(3),
+                seqlen_q=seqlen_q,
+                seqlen_k=seqlen_k,
+                dropout_p=ctx.dropout_p,
+                philox_seed=ctx.philox_seed,
+                philox_offset_base=ctx.philox_offset,
+                BLOCK_M=DQ_BLOCK_M, BLOCK_N=BLOCK,
+                BLOCK_DMODEL=ctx.BLOCK_DMODEL,
+                CAUSAL=ctx.causal,
+                num_warps=4, waves_per_eu=1,
+                num_stages=1,
+                ENABLE_DROPOUT=ctx.dropout_p > 0.0,
+            )
+        else:
+            dq = None
+        # print(h.asm["ttgir"])
+        return dq, dk, dv, None, None, None, None, None, None
 
 attention = _attention.apply
