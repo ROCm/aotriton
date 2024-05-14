6035fb4aa021d39a18adce883e059d5b99a8192c
Xinya Zhang <Xinya.Zhang@amd.com>
2024-05-14 13:42:56 -0500

    [mGPU] Run hipModuleLoadDataEx for each GPU device. (#24)
    This fixes TritonKernel::invoke for multi-GPU scenarios.
    
    hipModuleLoadDataEx only loads the image into a module that is specific to
    current GPU. Hence if another TritonKernel::invoke was made on a different GPU,
    the function call will fail since the module is only valid for the previous
    GPU.

--- include/aotriton/_internal/triton_kernel.h
+++ include/aotriton/_internal/triton_kernel.h
@@ -11,6 +11,9 @@
 
 #include "../runtime.h"
 #include <vector>
+#include <unordered_map>
+#include <shared_mutex>
+#include <tuple>
 
 namespace aotriton {
 
@@ -24,11 +27,24 @@ public:
   void clear_decompressed_image();
 #endif
 private:
+  std::tuple<hipFunction_t, hipError_t> load_for_device(int device_id, const char* kernel_name);
+  hipFunction_t cfind_function(int device_id) const;
+
   const void* kernel_image_ = nullptr;
   size_t image_size_ = 0;
+  struct DeviceFunction {
+    DeviceFunction(int device_id_,
+                   hipModule_t mod_,
+                   hipFunction_t func_);
+    ~DeviceFunction();
+    int device_id = -1;
+    hipModule_t mod = nullptr;
+    hipFunction_t func = nullptr; 
+  };
+  std::unordered_map<int, DeviceFunction> funcache_;
+  std::shared_mutex mutex_;
+
   dim3 block_ { 256, 1, 1 };
-  hipModule_t mod_ = nullptr;
-  hipFunction_t fun_ = nullptr;
   int shared_memory_size_;
 #if AOTRITON_USE_ZSTD
   std::vector<char> decompressed_kernel_image_;
