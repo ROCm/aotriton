a820496cdffe94ce1115a1947a4e106752735c1c
Xinya Zhang <Xinya.Zhang@amd.com>
2024-06-19 18:32:30 +0000

    Port to performant kernel and moving away from block pointers for tl.store

--- tritonsrc/fwd_kernel_inner.py
+++ tritonsrc/fwd_kernel_inner.py
@@ -5,148 +5,134 @@ import triton
 import triton.language as tl
 from dropout import dropout_mask, dropout_rng, dropout_offsets
 
+# Convenience function to load with optional boundary checks.
+# "First" is the major dim, "second" is the minor dim.
 @triton.jit
-def max_fn(x, y):
-    return tl.math.max(x, y)
+def load_fn(ptrs, offset_first, offset_second, boundary_first, boundary_second):
+    if offset_first is not None and offset_second is not None:
+        mask = (offset_first[:, None] < boundary_first) & \
+               (offset_second[None, :] < boundary_second)
+        tensor = tl.load(ptrs, mask=mask, other=0.0)
+    elif offset_first is not None:
+        mask = offset_first[:, None] < boundary_first
+        tensor = tl.load(ptrs, mask=mask, other=0.0)
+    elif offset_second is not None:
+        mask = offset_second[None, :] < boundary_second
+        tensor = tl.load(ptrs, mask=mask, other=0.0)
+    else:
+        tensor = tl.load(ptrs)
+    return tensor
 
 @triton.jit
 def attn_fwd_inner(
-    acc, l_i, m_i, q,
-    K_block_ptr, V_block_ptr, B_block_ptr,
-    start_m,
-    seqlen_q,
-    q_padded,
-    seqlen_k_low,
-    seqlen_k_high,
-    k_padded,
-    dropout_p,
-    dropout_seqlen_k,
-    philox_seed,
-    batch_philox_offset,
-    encoded_softmax_block_ptr,
-    BLOCK_M: tl.constexpr,
-    BLOCK_DMODEL: tl.constexpr,
-    BLOCK_N: tl.constexpr,
-    CAUSAL: tl.constexpr,
-    offs_m: tl.constexpr,
-    offs_n: tl.constexpr,
-    pre_load_v: tl.constexpr,
-    ENABLE_DROPOUT: tl.constexpr,
-    RETURN_ENCODED_SOFTMAX: tl.constexpr,
-    MARGINAL_BLOCK: tl.constexpr,  # MARGINAL_BLOCK = CAUSAL or k_padded
-    PADDED_HEAD: tl.constexpr,
-    BIAS_TYPE: tl.constexpr,
+        # Problem Description
+        acc, l_i, m_i,
+        q, k_ptrs, v_ptrs, bias_ptrs,
+        stride_kn, stride_vk, stride_bn,
+        seqlen_q, seqlen_k, head_dim,
+        # Sub-problem range
+        start_m, block_min, block_max,
+        # Auxiliary options
+        ## Dropout
+        dropout_p, philox_seed, batch_philox_offset, max_seqlen_k,
+        ## Debug Return
+        encoded_sm_ptrs,
+        ## Irregular support
+        offs_n_causal, masked_blocks, n_extra_tokens,
+        ## Alibi
+        alibi_slope,
+        # constexpr starts here
+        CAUSAL: tl.constexpr,
+        BLOCK_M: tl.constexpr,
+        BLOCK_DMODEL: tl.constexpr,
+        BLOCK_N: tl.constexpr,
+        PRE_LOAD_V: tl.constexpr,
+        MASK_STEPS: tl.constexpr,
+        ENABLE_DROPOUT: tl.constexpr,
+        RETURN_ENCODED_SOFTMAX: tl.constexpr,
+        PADDED_HEAD: tl.constexpr,
 ):
-    lo, hi = seqlen_k_low, seqlen_k_high
-    if MARGINAL_BLOCK:
-        K_block_ptr = tl.advance(K_block_ptr, (0, lo))
-        V_block_ptr = tl.advance(V_block_ptr, (lo, 0))
-        if RETURN_ENCODED_SOFTMAX:
-            encoded_softmax_block_ptr = tl.advance(encoded_softmax_block_ptr, (0, lo))
-        if BIAS_TYPE == 1:
-            B_block_ptr = tl.advance(B_block_ptr, (0, lo))
-    # loop over k, v and update accumulator
-    for start_n in range(lo, hi, BLOCK_N):
-        # -- compute qk ----
-        # MARGINAL_BLOCK serves as a compile-time switch for first attn_fwd_inner calls to "solid" blocks
-        k = tl.load(K_block_ptr, boundary_check=(1,0), padding_option="zero")
-        '''
-        if MARGINAL_BLOCK and k_padded:
-            if PADDED_HEAD:
-                k = tl.load(K_block_ptr, boundary_check=(1,0), padding_option="zero")
-            else:
-                k = tl.load(K_block_ptr, boundary_check=(1,), padding_option="zero")
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    # loop over k, v, and update accumulator
+    for start_n in range(block_min, block_max, BLOCK_N):
+        # For padded blocks, we will overrun the tensor size if
+        # we load all BLOCK_N. For others, the blocks are all within range.
+        if MASK_STEPS:
+            k_offs_n = start_n + tl.arange(0, BLOCK_N)
         else:
-            if PADDED_HEAD:
-                k = tl.load(K_block_ptr, boundary_check=(0,), padding_option="zero")
-            else:
-                k = tl.load(K_block_ptr)
-        if pre_load_v:
-            if MARGINAL_BLOCK and k_padded:
-                if PADDED_HEAD:
-                    v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
-                else:
-                    v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
-            else:
-                if PADDED_HEAD:
-                    v = tl.load(V_block_ptr, boundary_check=(1,), padding_option="zero")
-                else:
-                    v = tl.load(V_block_ptr)
-        '''
-        if pre_load_v:
-            v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
+            k_offs_n = None
+        k_offs_d = None if not PADDED_HEAD else tl.arange(0, BLOCK_DMODEL)
+        k = load_fn(k_ptrs, k_offs_d, k_offs_n, head_dim, seqlen_k)
+        if PRE_LOAD_V:
+            # We can use the same offsets as k, just with dims transposed.
+            v = load_fn(v_ptrs, k_offs_n, k_offs_d, seqlen_k, head_dim)
         qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-        if MARGINAL_BLOCK:
-            if CAUSAL:
-                mask = offs_m[:, None] >= (start_n + offs_n[None, :])
+        # We start from end of seqlen_k so only the first iteration would need
+        # to be checked for padding if it is not a multiple of block_n
+        # TODO: This can be optimized to only be true for the padded block.
+        if MASK_STEPS:
+            # If this is the last block / iteration, we want to
+            # mask if the sequence length is not a multiple of block size
+            # a solution is to always do BLOCK_M // BLOCK_N + 1 steps if not is_modulo_mn.
+            # last step might get wasted but that is okay. check if this masking works For
+            # that case.
+            if (start_n + BLOCK_N == block_max) and (n_extra_tokens != 0):
+                boundary_m = tl.full([BLOCK_M], seqlen_k, dtype=tl.int32)
+                size_n = start_n + offs_n[None, :]
+                mask = size_n < boundary_m[:, None]
                 qk = tl.where(mask, qk, float("-inf"))
-            if k_padded:
-                boundary_m = tl.full([BLOCK_M], seqlen_k_high, dtype=tl.int32)
-                size_n = start_n + offs_n[None,:]
-                mask = size_n < boundary_m[:,None]
-                qk = tl.where(mask, qk, float("-inf"))
-        if BIAS_TYPE == 0:
-            pass
-        elif BIAS_TYPE == 1:
-            bias = tl.load(B_block_ptr, boundary_check=(0,1), padding_option="zero")
-            '''
-            if q_padded and k_padded:  # CAVEAT: using "or" disables the partial boundary_check branches
-                bias = tl.load(B_block_ptr, boundary_check=(0,1), padding_option="zero")
-            elif q_padded:
-                bias = tl.load(B_block_ptr, boundary_check=(0,), padding_option="zero")
-            elif k_padded:
-                bias = tl.load(B_block_ptr, boundary_check=(1,), padding_option="zero")
-            else:
-                bias = tl.load(B_block_ptr)
-            '''
-            qk += bias * 1.44269504089
-        else:
-            tl.static_assert(False, f'Unsupported BIAS_TYPE {BIAS_TYPE}')
+        if CAUSAL:
+            causal_boundary = start_n + offs_n_causal
+            causal_mask = offs_m[:, None] >= causal_boundary[None, :]
+            qk = tl.where(causal_mask, qk, float("-inf"))
+        # -- compute qk ----
         qk += tl.dot(q, k)
+        if bias_ptrs is not None:
+            bias_offs_n = start_n + tl.arange(0, BLOCK_N) if MASK_STEPS else None
+            bias = load_fn(bias_ptrs, offs_m, bias_offs_n, seqlen_q, seqlen_k)
+            # While bias is added after multiplying qk with sm_scale,
+            # our optimization to use 2^x instead of e^x results in an additional
+            # scale factor of log2(e) which we must also multiply the bias with.
+            qk += (bias * 1.44269504089)
+
+        if alibi_slope is not None:
+            # Compute the global position of each token within the sequence
+            global_m_positions = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+            global_n_positions = start_n + tl.arange(0, BLOCK_N)
+            alibi_block = compute_alibi_block(alibi_slope, seqlen_q, seqlen_k, global_m_positions,
+                                              global_n_positions)
+            qk += (alibi_block * 1.44269504089)  # scale factor of log2(e)
+
+        # softmax
         m_ij = tl.maximum(m_i, tl.max(qk, 1))
         qk = qk - m_ij[:, None]
         p = tl.math.exp2(qk)
+
         # CAVEAT: Must update l_ij before applying dropout
         l_ij = tl.sum(p, 1)
-        # Note about the conflicts of Flash attention algorithm and PyTorch's CUDA implementation
-        # PyTorch needs to return softmax(qk) (dropout mask encoded in sign bits)
-        # While Flash attention paper computer the dropout AFTER exp2(qk- m_ij)
         if ENABLE_DROPOUT:
-            philox_offset = batch_philox_offset + start_m * BLOCK_M * dropout_seqlen_k + start_n
-            keep = dropout_mask(philox_seed, philox_offset, dropout_p, BLOCK_M, BLOCK_N, dropout_seqlen_k)
+            philox_offset = batch_philox_offset + start_m * BLOCK_M * max_seqlen_k + start_n
+            keep = dropout_mask(philox_seed, philox_offset, dropout_p, BLOCK_M, BLOCK_N, max_seqlen_k)
             if RETURN_ENCODED_SOFTMAX:
-                tl.store(encoded_softmax_block_ptr, tl.where(keep, p, -p).to(encoded_softmax_block_ptr.type.element_ty), boundary_check=(0,1))
+                tl.store(encoded_sm_ptrs, tl.where(keep, p, -p).to(q.type.element_ty))
             p = tl.where(keep, p, 0.0)
         elif RETURN_ENCODED_SOFTMAX:
-            tl.store(encoded_softmax_block_ptr,
-                     p.to(encoded_softmax_block_ptr.type.element_ty),
-                     boundary_check=(0,1))
+            tl.store(encoded_sm_ptrs, p.to(q.type.element_ty))
         # -- update output accumulator --
         alpha = tl.math.exp2(m_i - m_ij)
         acc = acc * alpha[:, None]
-        if not pre_load_v:
-            v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
-            '''
-            if MARGINAL_BLOCK and k_padded:
-                if PADDED_HEAD:
-                    v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
-                else:
-                    v = tl.load(V_block_ptr, boundary_check=(0,1), padding_option="zero")
-            else:
-                if PADDED_HEAD:
-                    v = tl.load(V_block_ptr, boundary_check=(1,), padding_option="zero")
-                else:
-                    v = tl.load(V_block_ptr)
-            '''
+        if not PRE_LOAD_V:
+            v = load_fn(v_ptrs, k_offs_n, k_offs_d, seqlen_k, head_dim)
         # -- update m_i and l_i
         l_i = l_i * alpha + l_ij
         # update m_i and l_i
         m_i = m_ij
-        acc += tl.dot(p.to(V_block_ptr.type.element_ty), v)
-        V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
-        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
+        acc += tl.dot(p.to(v.type.element_ty), v)
+        k_ptrs += BLOCK_N * stride_kn
+        v_ptrs += BLOCK_N * stride_vk
+        if bias_ptrs is not None:
+            bias_ptrs += BLOCK_N * stride_bn
         if RETURN_ENCODED_SOFTMAX:
-            encoded_softmax_block_ptr = tl.advance(encoded_softmax_block_ptr, (0, BLOCK_N))
-        if BIAS_TYPE == 1:
-            B_block_ptr = tl.advance(B_block_ptr, (0, BLOCK_N))
+            encoded_sm_ptrs += BLOCK_N
     return acc, l_i, m_i
