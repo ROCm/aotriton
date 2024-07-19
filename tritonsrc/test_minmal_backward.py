ac2b73c14e8e72ec6412e42aea1e624932e82eda
Xinya Zhang <Xinya.Zhang@amd.com>
2024-07-19 01:11:26 +0000

    Fix batch != 1 or nheads != 1 cases

--- tritonsrc/test_minmal_backward.py
+++ tritonsrc/test_minmal_backward.py
@@ -8,8 +8,8 @@ import os
 
 from _common_backward import _do_test_op_bwd
 
-@pytest.mark.parametrize('BATCH', [1])
-@pytest.mark.parametrize('N_HEADS', [1])
+@pytest.mark.parametrize('BATCH', [1, 3])
+@pytest.mark.parametrize('N_HEADS', [1, 5])
 @pytest.mark.parametrize('D_HEAD', [16])
 @pytest.mark.parametrize('seqlen_q', [16, 32])
 @pytest.mark.parametrize('seqlen_k', [16, 32])
