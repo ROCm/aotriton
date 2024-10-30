23b4b863ecf3c9e729c0e99bf2fa73add1863e56
Xinya Zhang <Xinya.Zhang@amd.com>
2024-10-30 17:52:27 +0000

    TRITON_HASH -> TRITON_LLVM_HASH

--- dockerfile/build.sh
+++ dockerfile/build.sh
@@ -14,28 +14,26 @@ OUTPUT_DIR="$3"
 AOTRITON_GIT_NAME="$4"
 AOTRITON_TARGET_GPUS="$5"
 
-if [ -z ${TRITON_HASH+x} ]; then
+if [ -z ${TRITON_LLVM_HASH+x} ]; then
   echo "Guessing Triton's Hash from AOTriton's Git tag"
   case "${AOTRITON_GIT_NAME}" in
     0.7b)
-      TRITON_HASH="657ec732"
+      TRITON_LLVM_HASH="657ec732"
       ;;
     0.7.*b)
-      TRITON_HASH="657ec732"
+      TRITON_LLVM_HASH="657ec732"
       ;;
     0.8b)
-      TRITON_HASH="b5cc222d"
+      TRITON_LLVM_HASH="b5cc222d"
       ;;
     *)
-      echo "Cannot guess Triton's hash from tag. Set env var TRITON_HASH to proceed"
-      echo 'The value can be get from `head -c 8 third_party/triton/cmake/llvm-hash.txt`'
-      echo '(Must switch branch and update submodule first)'
+      echo "Only 0.7b, 0.7.1b and 0.8b are supported right now"
       exit
       ;;
   esac
 fi
 
-DOCKER_IMAGE=aotriton:manylinux_2_28-buildenv-tiny  # TODO: FIXME
+DOCKER_IMAGE=aotriton:manylinux_2_28-buildenv-tiny
 
 if [ -z "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" ]; then
   if [ -z ${AMDGPU_INSTALLER+x} ]; then
@@ -54,7 +52,7 @@ else
   workspace_option2="type=bind,target=/root/build,source=$(realpath $WORKSPACE)"
 fi
 
-echo /input/install.sh ${TRITON_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}"
+echo /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}"
 
 docker run --mount "type=bind,source=$(realpath ${INPUT_DIR}),target=/input" \
   --mount "type=bind,source=$(realpath ${OUTPUT_DIR}),target=/output" \
@@ -62,4 +60,4 @@ docker run --mount "type=bind,source=$(realpath ${INPUT_DIR}),target=/input" \
   -w / \
   -it ${DOCKER_IMAGE} \
   bash \
-  /input/install.sh ${TRITON_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}"
+  /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}"
--- dockerfile/input/install_triton.sh
+++ dockerfile/input/install_triton.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TRITON_HASH="$1"
-fn="llvm-${TRITON_HASH}-almalinux-x64.tar.gz"
+TRITON_LLVM_HASH="$1"
+fn="llvm-${TRITON_LLVM_HASH}-almalinux-x64.tar.gz"
 
 if [ -f "/input/$fn" ]; then
   mkdir -p "$HOME/.triton/llvm"
   cd "$HOME/.triton/llvm"
   echo "Unpacking $fn" && tar xf "/input/$fn"
-  echo -n "https://oaitriton.blob.core.windows.net/public/llvm-builds/$fn" > "llvm-${TRITON_HASH}-almalinux-x64/version.txt"
+  echo -n "https://oaitriton.blob.core.windows.net/public/llvm-builds/$fn" > "llvm-${TRITON_LLVM_HASH}-almalinux-x64/version.txt"
 fi
