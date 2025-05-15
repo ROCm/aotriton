e22340f8b5802d62c80ff57ee5d934b17a950977
Xinya Zhang <Xinya.Zhang@amd.com>
2025-05-15 16:05:31 -0500

    Fix the algorithm

--- docs/windowed_attention.py
+++ docs/windowed_attention.py
@@ -3,14 +3,22 @@
 import numpy as np
 import itertools
 from dataclasses import dataclass
+import pytest
+
+np.set_printoptions(linewidth=300)
 
 def cdiv(x: int, y: int):
     return (x + y - 1) // y
+
+def fdiv(x: int, y: int):
+    return x // y
 
 @dataclass
 class FillerInfo:
     lb_lo             : int = 0
     lb_hi             : int = 0
+    fb_lo             : int = 0
+    fb_hi             : int = 0
     rb_lo             : int = 0
     rb_hi             : int = 0
     # leading_empty   : int = 0
@@ -26,26 +34,43 @@ class FillerInfo:
 def close_interval_isect(I_a, I_b):
     a_s, a_e = I_a
     b_s, b_e = I_b
-    if b_s < a_e or a_s > b_e:
+    if a_s > a_e or b_s > a_e or a_s > b_e:
+        # print(f'Isect {I_a=} {I_b=} = Empty Set')
         return 0, -1
     o_s = max(a_s, b_s)
-    o_e = max(a_e, b_e)
+    o_e = min(a_e, b_e)
+    # print(f'Isect {I_a=} {I_b=} = [{o_s}, {o_e}]')
     return [o_s, o_e]
 
 class Validator:
-    def __init__(self, ref_mask, window_lef, window_right):
+    def __init__(self, ref_mask, window_left, window_right):
         self._ref_mask = ref_mask
         self._window_left = window_left
         self._window_right = window_right
         self._seqlen_q, self._seqlen_k = self._ref_mask.shape
 
     def validate(self, BLOCK_M, BLOCK_N):
+        # print(f'window_left = {self._window_left} window_right = {self._window_right} {BLOCK_M=} {BLOCK_N=}')
         mask = np.full_like(self._ref_mask, 2)
         for start_m in range(0, self._seqlen_q, BLOCK_M):
             filler_info = self.create_filler(start_m, BLOCK_M, BLOCK_N)
+            # print(f'{start_m=} {filler_info=}')
             mask[start_m:start_m+BLOCK_M] = self.line_fill(start_m, filler_info)
+        # pt(mask)
+        is_allclose = np.allclose(mask, self._ref_mask)
+        if not is_allclose:
+            for start_m in range(0, self._seqlen_q, BLOCK_M):
+                filler_info = self.create_filler(start_m, BLOCK_M, BLOCK_N, verbose=True)
+                print(f'{start_m=} {filler_info=}')
+                mask[start_m:min(start_m+BLOCK_M, self._seqlen_q)] = self.line_fill(start_m, filler_info, verbose=True)
+                pt(mask[start_m:min(start_m+BLOCK_M, self._seqlen_q)], pfx=f'Mask at {start_m:05}: ')
+            assert False, f"seqlen_q={self._seqlen_q} seqlen_k={self._seqlen_k} window_left = {self._window_left} window_right = {self._window_right} {BLOCK_M=} {BLOCK_N=}\nRefMask: {np.array2string(self._ref_mask, prefix='RefMask: ')}\nOutMask: {np.array2string(mask, prefix='OutMask: ')}"
 
-    def create_filler(self, start_m, BLOCK_M, BLOCK_N):
+    def create_filler(self, start_m, BLOCK_M, BLOCK_N, *, verbose=False):
+        seqlen_q = self._seqlen_q
+        seqlen_k = self._seqlen_k
+        window_left = self._window_left
+        window_right = self._window_right
         # Main diagnoal: M = N
         # Window Left line: M - window_left = N
         # Window Right line: M = N - window_right
@@ -53,40 +78,69 @@ class Validator:
         # Intersection point (N axis) b/w window left line and M = start_m
         # start_m - window_left = N
         isec_lo = start_m - window_left
-        # Intersection point (N axis) b/w window left line and M = start_m + BLOCK_M
-        # M = + window_left + N = start_m + BLOCK_M
-        isec_hi = start_m + BLOCK_M - window_left
-        lsec_lob = cdiv(isec_lo, BLOCK_N)
-        lsec_hib = cdiv(isec_hi, BLOCK_N)
+        # Intersection point (N axis) b/w window left line and M = min(start_m + BLOCK_M, seqlen_q)
+        # M = + window_left + N = min(start_m + BLOCK_M, seqlen_q)
+        isec_hi = min(start_m + BLOCK_M, seqlen_q) - window_left
+        lsec_lob = fdiv(isec_lo, BLOCK_N)
+        lsec_hib = fdiv(isec_hi, BLOCK_N)
+        if verbose:
+            print(f'{window_left=} {isec_lo=} {isec_hi=} {lsec_lob=} {lsec_hib=}')
 
         # Intersection point (N axis) b/w window left line and M = start_m
         # M = N - window_right = start_m
         isec_lo = start_m + window_right
-        # Intersection point (N axis) b/w window left line and M = start_m + BLOCK_M
-        # M = N - window_right = start_m + BLOCK_M
-        isec_hi = start_m + BLOCK_M + window_right
-        rsec_lob = cdiv(isec_lo, BLOCK_N)
-        rsec_hib = cdiv(isec_hi, BLOCK_N)
+        # Intersection point (N axis) b/w window left line and M = min(start_m + BLOCK_M, seqlen_q)
+        # M = N - window_right = min(start_m + BLOCK_M, seqlen_q)
+        isec_hi = min(start_m + BLOCK_M, seqlen_q) + window_right
+        rsec_lob = fdiv(isec_lo, BLOCK_N)
+        rsec_hib = fdiv(isec_hi, BLOCK_N)
+        if verbose:
+            print(f'{window_right=} {isec_lo=} {isec_hi=} {rsec_lob=} {rsec_hib=}')
         # Leading Empty blocks      : [-inf, lsec_lob)
         # Leading Masked Blocks     : [lsec_lob, lsec_hib]
         # Solid Blocks              : (lsec_hib, rsec_lob) = [lsec_hib + 1, rsec_lob - 1]
         # Trailing Masked Blocks    : [rsec_lob, rsec_hib]
         # Trailing Empty Blocks     : [rsec_hib, +inf)
         #
-        # These blocks should be intersected with [0, cdiv(seqlen_k, BLOCK_N)]
+        # These blocks should be intersected with [0, fdiv(seqlen_k, BLOCK_N)]
         # https://scicomp.stackexchange.com/a/26260
 
-        valid_bs = [0, cdiv(seqlen_k, BLOCK_N)]
+        valid_bs = [0, cdiv(self._seqlen_k, BLOCK_N) -1]  # Closing interval
         lb_lo, lb_hi = close_interval_isect([lsec_lob, lsec_hib], valid_bs)
         rb_lo, rb_hi = close_interval_isect([rsec_lob, rsec_hib], valid_bs)
-        return FillerInfo(lb_lo=lb_lo,
-                          lb_hi=lb_hi,
-                          rb_lo=rb_lo,
-                          rb_hi=rb_hi,
-                          seqlen_q=self._seqlen_q,
-                          seqlen_k=self._seqlen_k,
-                          BLOCK_M=BLOCK_M,
-                          BLOCK_N=BLOCK_N)
+        if verbose:
+            print(f'{lsec_lob=} {lsec_hib=} isect {valid_bs=} -> {lb_lo=} {lb_hi=}')
+            print(f'{rsec_lob=} {rsec_hib=} isect {valid_bs=} -> {rb_lo=} {rb_hi=}')
+        # calc isec b/w lb and rb
+        ub_lo, ub_hi = close_interval_isect([lb_lo, lb_hi], [rb_lo, rb_hi])
+        if ub_lo > ub_hi:
+            fb_lo, fb_hi = close_interval_isect([lsec_hib+1, rsec_lob-1], valid_bs)
+            if verbose:
+                print(f'{lsec_hib+1=} {rsec_lob-1=} isect {valid_bs=} -> {fb_lo=} {fb_hi=}')
+            # lb not intersecting with rb
+            return FillerInfo(lb_lo=lb_lo,
+                              lb_hi=lb_hi,
+                              fb_lo=fb_lo,
+                              fb_hi=fb_hi,
+                              rb_lo=rb_lo,
+                              rb_hi=rb_hi,
+                              seqlen_q=self._seqlen_q,
+                              seqlen_k=self._seqlen_k,
+                              BLOCK_M=BLOCK_M,
+                              BLOCK_N=BLOCK_N)
+        else:
+            # lb intersecting with rb
+            # Unify the processing
+            return FillerInfo(lb_lo=lb_lo,
+                              lb_hi=rb_hi,
+                              fb_lo=-2,
+                              fb_hi=-3,
+                              rb_lo=-1,
+                              rb_hi=-2,
+                              seqlen_q=self._seqlen_q,
+                              seqlen_k=self._seqlen_k,
+                              BLOCK_M=BLOCK_M,
+                              BLOCK_N=BLOCK_N)
         # return FillerInfo(leading_empty=leading_empty,
         #                   leading_masked=leading_masked,
         #                   solid=solid,
@@ -97,34 +151,38 @@ class Validator:
         #                   BLOCK_M=BLOCK_M,
         #                   BLOCK_N=BLOCK_N)
 
-    def line_fill(self, start_m, info):
+    def line_fill(self, start_m, info, *, verbose=False):
         BLOCK_M = info.BLOCK_M
         BLOCK_N = info.BLOCK_N
-        seqlen_k_round = cdiv(seqlen_k, BLOCK_N) * BLOCK_N
-        mask = np.full_like((BLOCK_M, seqlen_k_round), 0, dtype=np.int8)
-        start_n = 0
-        # # Leading Empty
-        # for n in range(start_n, start_n + info.leading_empty * BLOCK_N, BLOCK_N):
-        #     mask[:, start_n:start_n+BLOCK_N] = 0
-        start_n += info.leading_empty * BLOCK_N
+        seqlen_k_round = cdiv(self._seqlen_k, BLOCK_N) * BLOCK_N
+        mask = np.zeros((BLOCK_M, seqlen_k_round), dtype=np.int8)
         # Leading Masked
         for n in range(info.lb_lo, info.lb_hi+1):
-            mask[:, n*BLOCK_N:n*BLOCK_N+BLOCK_N] = self.masked_block(info, start_m, start_n) # TODO: Partial Mask
-        start_n += info.leading_masked * BLOCK_N
+            blk = self.masked_block(info, start_m, n*BLOCK_N)
+            if verbose:
+                pt(blk, f' Block {n*BLOCK_N:05}')
+                pt(self._ref_mask[start_m:start_m+BLOCK_M, n*BLOCK_N:n*BLOCK_N+BLOCK_N], f'RefBlk {n*BLOCK_N:05}')
+            mask[:, n*BLOCK_N:n*BLOCK_N+BLOCK_N] = blk
+            if verbose:
+                pt(mask[start_m:start_m+BLOCK_M, n*BLOCK_N:n*BLOCK_N+BLOCK_N], f'Updated Block {n*BLOCK_N:05}')
         # Solid Blocks
-        for n in range(info.lb_hi+1, info.rb_lo):
+        for n in range(info.fb_lo, info.fb_hi+1):
+            if verbose:
+                print(f'Solid Block {n*BLOCK_N:05}')
             mask[:, n*BLOCK_N:n*BLOCK_N+BLOCK_N] = 1
-        start_n += info.solid * BLOCK_N
         # Trailing Masked
         for n in range(info.rb_lo, info.rb_hi+1):
-            mask[:, n*BLOCK_N:n*BLOCK_N+BLOCK_N] = self.masked_block(info, start_m, start_n) # TODO: Partial mask
-        start_n += info.trailing_masked * BLOCK_N
-        # # Trailing Empty
-        # for n in range(start_n, start_n + info.trailing_empty * BLOCK_N, BLOCK_N):
-        #     mask[:, start_n:start_n+BLOCK_N] = 0
-        start_n += info.trailing_empty * BLOCK_N
-        m = min(start_m + BLOCK_M, info.seqlen_q) - start_m
+            blk = self.masked_block(info, start_m, n*BLOCK_N)
+            if verbose:
+                pt(blk, f' Block {n*BLOCK_N:05}')
+                pt(self._ref_mask[start_m:start_m+BLOCK_M, n*BLOCK_N:n*BLOCK_N+BLOCK_N], f'RefBlk {n*BLOCK_N:05}')
+            mask[:, n*BLOCK_N:n*BLOCK_N+BLOCK_N] = blk
+        m = min(BLOCK_M, info.seqlen_q - start_m)
         n = info.seqlen_k
+        if verbose:
+            print(f'Return: {m=} {n=} {mask.shape=}')
+            pt(mask, '  Full Return: ')
+            pt(mask[:m, :n], 'Actual Return: ')
         return mask[:m, :n]
 
     # Note window left and window right lines may be in the same block
@@ -143,39 +201,17 @@ class Validator:
         def triu_mask(k):
             return MS[:, None] + k <= NS[None, :]
 
-        mask = tril_mask(info.window_right) & triu_mask(-info.window_left)
+        mask = (tril_mask(self._window_right) & triu_mask(-self._window_left)).astype(np.int8)
 
-        return None
+        return mask
 
-def pt(t):
-    print('Array:', np.array2string(t, prefix='Array: '))
-
-print('Demo np.tril and triu')
-ones = np.ones((8, 16), dtype=np.int8)
-pt(ones)
-window_left = 1
-window_right = 2
-
-win = np.tril(ones, window_right)
-pt(win)
-win = np.triu(win, -window_left)
-pt(win)
+def pt(t, pfx='Array:'):
+    print(pfx, np.array2string(t, prefix=pfx), sep='')
 
 def create_window_mask(M, N, window_left, window_right):
     ones = np.ones((M, N), dtype=np.int8)
     tmp = np.tril(ones, window_right)
     return np.triu(tmp, -window_left)
-
-print('Simulate bottom right aligned causal with windowed attention')
-pt(create_window_mask(8, 16, -8, 16))
-
-X = np.arange(0, 8)
-Y = np.arange(0, 16)
-mask = (X[:, None] <= Y[None, :]).astype(np.int8)
-
-pt(mask)
-
-print('Simulate bottom right aligned causal with simulated windowed attention with X[:, None] <= Y[None, :]')
 
 def sim_tril(m, k):
     X = np.arange(0, m.shape[0]) + k
@@ -187,50 +223,94 @@ def sim_triu(m, k):
     Y = np.arange(0, m.shape[1])
     return np.where(X[:, None] <= Y[None, :], m, 0)
 
-ones = np.ones((5, 7), dtype=np.int8)
-for i in range(-5, 7):
-    s = sim_tril(ones, i)
-    r = np.tril(ones, i)
-    print(f'Validate sim_tril(k={i}) {np.allclose(s, r)}')
-    pt(s)
-    pt(r)
-
-for i in range(-5, 7):
-    s = sim_triu(ones, i)
-    r = np.triu(ones, i)
-    print(f'Validate sim_triu(k={i}) {np.allclose(s, r)}')
-    pt(s)
-    pt(r)
-
 def create_window_mask_sim(M, N, window_left, window_right):
     ones = np.ones((M, N), dtype=np.int8)
     tmp = sim_tril(ones, window_right)
     return sim_triu(tmp, -window_left)
     # return sim_triu(ones, -window_left)
 
-pt(create_window_mask_sim(8, 16, -8, 16))
+def prelude():
+    print('Demo np.tril and triu')
+    ones = np.ones((8, 16), dtype=np.int8)
+    pt(ones)
+    window_left = 1
+    window_right = 2
 
-exit()
+    win = np.tril(ones, window_right)
+    pt(win)
+    win = np.triu(win, -window_left)
+    pt(win)
 
-print('Test calculation of leading_masked and trailing_masked')
+    print('Simulate bottom right aligned causal with windowed attention')
+    pt(create_window_mask(8, 16, -8, 16))
+
+    X = np.arange(0, 8)
+    Y = np.arange(0, 16)
+    mask = (X[:, None] <= Y[None, :]).astype(np.int8)
+
+    pt(mask)
+
+    print(f'Validating sim_tril')
+    ones = np.ones((5, 7), dtype=np.int8)
+    for i in range(-5, 7):
+        s = sim_tril(ones, i)
+        r = np.tril(ones, i)
+        if not np.allclose(s, r):
+            assert np.allclose(s, r)
+            print(f'Validate sim_tril(k={i})')
+            pt(s)
+            pt(r)
+    print(f'sim_tril Validated')
+
+    print(f'Validating sim_triu')
+    for i in range(-5, 7):
+        s = sim_triu(ones, i)
+        r = np.triu(ones, i)
+        if not np.allclose(s, r):
+            print(f'Validate sim_triu(k={i})')
+            pt(s)
+            pt(r)
+            assert False
+    print(f'sim_triu Validated')
+
+    print('Simulate bottom right aligned causal with simulated windowed attention with X[:, None] <=/>= Y[None, :]')
+
+    pt(create_window_mask_sim(8, 16, -8, 16))
+    print('create_window_mask(5, 7, 1, 0)')
+    pt(create_window_mask(5, 7, 1, 0))
+    print('create_window_mask_sim(5, 7, 1, 0)')
+    pt(create_window_mask_sim(5, 7, 1, 0))
 
 REGULAR_SEQLEN_Q = [ 2 ** i for i in range(2, 15) ]
 REGULAR_SEQLEN_K = [ 2 ** i for i in range(2, 15) ]
 
 PRIME_SEQLEN_Q = [11, 17, 37, 67, 157, 257, 523, 1033, 2063, 4919, 10601]
 PRIME_SEQLEN_K = [13, 31, 41, 71, 223, 337, 571, 1063, 2081, 5237, 11369]
-
 Qs = sorted(REGULAR_SEQLEN_Q + PRIME_SEQLEN_Q)
 Ks = sorted(REGULAR_SEQLEN_K + PRIME_SEQLEN_K)
 BMS = [16,32,64,128]
 BNS = [16,32,64,128]
 
-for seqlen_q, seqlen_k in itertools.product(Qs, Ks):
-    # print(f'{seqlen_q=} {seqlen_k=}')
+# if False:
+#     BMS = [16]
+#     BNS = [16]
+#     _validate_qkmask(5, 7, BMS, BNS)
+#     exit()
+
+def _validate_qkmask(seqlen_q, seqlen_k, BLOCK_M, BLOCK_N):
     for window_left, window_right in itertools.product(range(seqlen_q), range(seqlen_k)):
         ref_mask = create_window_mask(seqlen_q, seqlen_k, window_left, window_right)
-        # print(f'{window_left=} {window_right=}')
-        # pt(mask)
+        # print(f'Validating {window_left=} {window_right=}')
+        # pt(ref_mask)
         validator = Validator(ref_mask, window_left, window_right)
-        for BLOCK_M, BLOCK_N in itertools.product(BMS, BNS):
-            validator.validate(BLOCK_M, BLOCK_N)
+        validator.validate(BLOCK_M, BLOCK_N)
+
+@pytest.mark.parametrize("Q", Qs)
+@pytest.mark.parametrize("K", Ks)
+@pytest.mark.parametrize("BLOCK_M", BMS)
+@pytest.mark.parametrize("BLOCK_N", BNS)
+def test_qkmask(Q, K, BLOCK_M, BLOCK_N):
+    _validate_qkmask(Q, K, BLOCK_M, BLOCK_N)
+
+if __name__ == '__main__':
+    prelude()
