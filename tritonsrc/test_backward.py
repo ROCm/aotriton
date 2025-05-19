Fix the dkdv kernel with new causal

tritonsrc/test_backward.py
@@ -172,8 +172,8 @@ def main2():
     # Memo: False-0.0-dtype0-0.0-False-4-256-8-4-1
     # Memo: False-0.0-dtype0-0.0-False-4-256-8-1-4
     # False-1.2-dtype0-0.0-False-4-4-72-1-4
-    BATCH = 8
-    N_HEADS = 8
+    BATCH = 1
+    N_HEADS = 1
     seqlen_q = 32
     seqlen_k = 4
     D_HEAD = 16
@@ -182,7 +182,7 @@ def main2():
     # N_HEADS = 8
     # seqlen_q = 256
     # seqlen_k = 4
-    causal = False
+    causal = True
     sm_scale = 1.2
     dropout_p = 0.0
     dtype = torch.float16