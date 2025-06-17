e574efccf00258b17e0d72843eafdc7a87f38bb4
Xinya Zhang <Xinya.Zhang@amd.com>
2025-06-17 18:51:17 -0500

    Finalize 0.10b Development (#102)
    # Overview
    
    This contains necessary changes to build final release of AOTriton 0.10b.
    
    ## Major Changes
    
    * For `flash:attn_fwd` Triton kernels, `PADDED_HEAD` must be set to `True` if
      `bias` is supplied.
        + This problem can only be detected with PyTorch's UT, which can be
          triggered when `seqlen_q/k` is multiple of 8 (but not 16). The root
          cause is still under investigation.  
    
    ## Minor Changes
    
    The changes are all for build scripts under `dockerfile/`
    
    * Support `AMDGPU_INSTALLER` and `AMDGPU_INSTALLER_SELECT_VERSION` to select/set ROCM installer package.
    * Add `TRITON_LLVM_HASH` for 0.8b/0.9b/0.10b
    * Support build from forked repo with env var `AOTRITON_GIT_URL`
    * Allow delete docker container after complete by setting env var `DELETE_ONCE_COMPLETE_OPTION='--rm'`
    * Allow add `.shard{N}` to file name with env var `AOTRITON_TARBALL_SHARD=N`
    * Use `ninja install/strip` instead

--- dockerfile/build.sh
+++ dockerfile/build.sh
@@ -25,7 +25,7 @@ INPUT_DIR="$1"
 WORKSPACE="$2"
 OUTPUT_DIR="$3"
 AOTRITON_GIT_NAME="$4"
-AOTRITON_TARGET_GPUS="$5"
+AOTRITON_TARGET_ARCH="$5"
 
 if [ -z ${TRITON_LLVM_HASH+x} ]; then
   echo "Guessing Triton's Hash from AOTriton's Git tag"
@@ -39,26 +39,63 @@ if [ -z ${TRITON_LLVM_HASH+x} ]; then
     0.8b)
       TRITON_LLVM_HASH="bd9145c8"
       ;;
+    0.8.*b)
+      TRITON_LLVM_HASH="bd9145c8"
+      ;;
+    0.9b)
+      TRITON_LLVM_HASH="86b69c31"
+      ;;
+    0.10b)
+      TRITON_LLVM_HASH="3c709802"
+      ;;
     *)
-      echo "Only 0.7b, 0.7.xb and 0.8b are supported right now"
+      echo "Unknown AOTRITON_GIT_NAME ${AOTRITON_GIT_NAME}. Please set TRITON_LLVM_HASH explicitly."
       exit
       ;;
   esac
 fi
 
+if [ -z ${AOTRITON_GIT_URL+x} ]; then
+  AOTRITON_GIT_URL="https://github.com/ROCm/aotriton.git"
+fi
+
 if [ -z ${NOIMAGE_MODE+x} ]; then
+  # default value
   NOIMAGE_MODE="OFF"
 fi
 
-DOCKER_IMAGE=aotriton:manylinux_2_28-buildenv-tiny
+if [ -z ${DELETE_ONCE_COMPLETE_OPTION+x} ]; then
+  # default value
+  DELETE_ONCE_COMPLETE_OPTION=""
+fi
+
+if [ -z ${AOTRITON_TARBALL_SHARD+x} ]; then
+  # default value
+  AOTRITON_TARBALL_SHARD_OPTION=""
+else
+  AOTRITON_TARBALL_SHARD_OPTION="--env AOTRITON_TARBALL_SHARD=${AOTRITON_TARBALL_SHARD}"
+fi
+
+if [ -z ${DOCKER_IMAGE+x} ]; then
+  DOCKER_IMAGE=aotriton:manylinux_2_28-buildenv-tiny
+fi
 
 if [ -z "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" ]; then
   if [ -z ${AMDGPU_INSTALLER+x} ]; then
-    build_options="--build-arg amdgpu_installer=${AMDGPU_INSTALLER}"
-  else
     build_options=""
+  else
+    build_options="--build-arg amdgpu_installer=${AMDGPU_INSTALLER}"
   fi
-  docker build ${build_options} -t ${DOCKER_IMAGE} -f manylinux_2_28.Dockerfile .
+  if [ -z ${AMDGPU_INSTALLER_SELECT_VERSION+x} ]; then
+    version_selection="true"
+  else
+    version_selection="${AMDGPU_INSTALLER_SELECT_VERSION}"
+  fi
+  echo "docker build ${build_options} ${version_selection} -t ${DOCKER_IMAGE} -f manylinux_2_28.Dockerfile ."
+  docker build ${build_options} \
+    --build-arg amdgpu_installer_select_version="${version_selection}" \
+    -t ${DOCKER_IMAGE} \
+    -f manylinux_2_28.Dockerfile .
 fi
 
 if [ "$WORKSPACE" == "tmpfs" ]; then
@@ -69,12 +106,16 @@ else
   workspace_option2="type=bind,target=/root/build,source=$(realpath $WORKSPACE)"
 fi
 
-echo /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}"
+echo /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_ARCH}"
 
 docker run --mount "type=bind,source=$(realpath ${INPUT_DIR}),target=/input" \
   --mount "type=bind,source=$(realpath ${OUTPUT_DIR}),target=/output" \
   ${workspace_option1} "${workspace_option2}" \
   -w / \
-  -it ${DOCKER_IMAGE} \
+  --env AOTRITON_GIT_URL=${AOTRITON_GIT_URL} \
+  ${AOTRITON_TARBALL_SHARD_OPTION} \
+  ${DELETE_ONCE_COMPLETE_OPTION} \
+  -it \
+  ${DOCKER_IMAGE} \
   bash \
-  /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_GPUS}" ${NOIMAGE_MODE}
+  /input/install.sh ${TRITON_LLVM_HASH} ${AOTRITON_GIT_NAME} "${AOTRITON_TARGET_ARCH}" ${NOIMAGE_MODE}
