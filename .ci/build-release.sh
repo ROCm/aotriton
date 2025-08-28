35b0069946c19dcdf35e31943277f839ce68b0c5
Xinya Zhang <Xinya.Zhang@amd.com>
2025-08-27 21:16:21 -0500

    .ci/build-release.sh: allow selection of arches

--- .ci/build-release.sh
+++ .ci/build-release.sh
@@ -5,11 +5,22 @@ if [ -z "$BASH_VERSION" ]; then
   exit 1
 fi
 
+if [ "$#" -lt 1 ]; then
+  echo 'Missing arguments. Usage: build-release.sh <noimage mode> [list of arches]' >&2
+  exit 1
+fi
+
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 . "${SCRIPT_DIR}/common-vars.sh"
 
 python_exec="/usr/bin/python3.11"
 noimage="$1"
+
+if [ "$#" -ge 2 ]; then
+  target_arch="$2"
+else
+  target_arch="${default_target_arch}"
+fi
 
 bdir="build"
 mkdir -p "${SCRIPT_DIR}/../${bdir}"
@@ -20,7 +31,7 @@ cmake .. -DCMAKE_PREFIX_PATH=/opt/rocm \
   -DCMAKE_INSTALL_PREFIX=installed_dir/aotriton \
   -DCMAKE_BUILD_TYPE=Release \
   -DAOTRITON_GPU_BUILD_TIMEOUT=0 \
-  "-DAOTRITON_TARGET_ARCH=${default_target_arch}" \
+  "-DAOTRITON_TARGET_ARCH=${target_arch}" \
   -DAOTRITON_NO_PYTHON=ON \
   -DAOTRITON_NOIMAGE_MODE=${noimage} \
   -G Ninja && ninja install/strip
