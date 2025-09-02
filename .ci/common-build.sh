e739834fe6de99a1871414153a1daf84bd8941c3
Xinya Zhang <Xinya.Zhang@amd.com>
2025-09-02 15:14:53 -0500

    .ci: default to mold in build-shim/build-test for faster linking

--- .ci/common-build.sh
+++ .ci/common-build.sh
@@ -4,34 +4,37 @@ SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 . "${SCRIPT_DIR}/common-vars.sh"
 
 function _common_build() {
-  target_arch="$1"
-  build_type="$2"
-  cmake_option0="$3"
-  bdir="build-${aotriton_major}.${aotriton_minor}-${build_type}-${target_arch}"
+  build_type="$1"
+  suffix="$2"
+  target_arch="$3"
+  build_for="$4"
+  shift 4
+  bdir="build-${aotriton_major}.${aotriton_minor}-${build_for}-${target_arch}"
   mkdir -p ${SCRIPT_DIR}/../${bdir}
   (
     cd ${SCRIPT_DIR}/../${bdir};
     cmake .. -DCMAKE_INSTALL_PREFIX=./install_dir \
-      -DCMAKE_BUILD_TYPE=$4 \
+      -DCMAKE_BUILD_TYPE=${build_type} \
       -DAOTRITON_TARGET_ARCH=${target_arch} \
-      ${cmake_option0} \
-      -DAOTRITON_NAME_SUFFIX=123 -G Ninja;
+      -DAOTRITON_NAME_SUFFIX=${suffix} \
+      "$@" \
+      -G Ninja;
     ninja install/strip
   )
 }
 
 function common_build() {
-  if [ "$#" -ne 3 ]; then
-    echo 'common_build expects 3 arguments: <target arch> <build type> <cmake option>' >&2
+  if [ "$#" -lt 2 ]; then
+    echo 'common_build expects at least 2 arguments: <target arch> <build for> [cmake options...]' >&2
     exit 1
   fi
-  _common_build "$@" Release
+  _common_build Release "123" "$@"
 }
 
 function debug_build() {
-  if [ "$#" -ne 3 ]; then
-    echo 'common_build expects 3 arguments: <target arch> <build type> <cmake option>' >&2
+  if [ "$#" -lt 2 ]; then
+    echo 'common_build expects at least 2 arguments: <target arch> <build for> [cmake options..]' >&2
     exit 1
   fi
-  _common_build "$@" Debug
+  _common_build Debug "123" "$@"
 }
