04a779a1b9204bcd5e118362e7714e2ca5ecd300
Xinya Zhang <Xinya.Zhang@amd.com>
2025-09-18 19:04:53 -0400

    v3python/celery: Fix tasks. single worker testing passed.

--- v3python/celery/tasks.py
+++ v3python/celery/tasks.py
@@ -2,6 +2,7 @@ from .celery import app
 from celery.signals import celeryd_after_setup, celeryd_init
 from celery import current_task, chain, group, chord
 import os
+import time
 import logging
 import sys
 import socket
@@ -10,13 +11,13 @@ from pathlib import Path
 LOCALQ = socket.gethostname() + '_localqueue'
 
 def _stub_probe_nhsaco(kname):
-    if kname == 'attn_fwd':
-        return 32
-    if kname == 'bwd_kernel_dk_dv':
-        return 12
-    if kname == 'bwd_kernel_dq':
-        return 20
-    return 7
+    # if kname == 'attn_fwd':
+    #     return 32
+    # if kname == 'bwd_kernel_dk_dv':
+    #     return 12
+    # if kname == 'bwd_kernel_dq':
+    #     return 20
+    return 3
 
 probe_nhsaco = _stub_probe_nhsaco
 
@@ -34,21 +35,25 @@ def xsum(numbers):
 
 @app.task
 def preprocess(task_config):
-    task_id = task_config.task_id
-    Path('/dev/shm/aotriton-tuner-{task_id}').mkdir()
+    godel_number = task_config["godel_number"]
+    p = Path('/dev/shm/aotriton-tuner') / str(godel_number)
+    p.mkdir(parents=True, exist_ok=False)  # exist_ok=False for debugging
+    return task_config
 
 @app.task
 def postprocess(task_config):
-    task_id = task_config.task_id
+    godel_number = task_config["godel_number"]
+    p = Path('/dev/shm/aotriton-tuner') / str(godel_number)
     # Note: Real tuning uses rm -rf not rmdir
     #       Using rmdir is to confirm all sub tasks complete (otherwise error)
-    Path('/dev/shm/aotriton-tuner-{task_id}').rmdir()
+    p.rmdir()
 
 @app.task
 def tune_hsaco(task_config, kname, hsaco_id):
-    fn = '/dev/shm/aotriton-tuner-{task_id}/{kname}-{hsaco_id}.run'
+    godel_number = task_config["godel_number"]
+    fn = f'/dev/shm/aotriton-tuner/{godel_number}-{kname}-{hsaco_id}.run'
     Path(fn).touch()
-    os.sleep(0.1)
+    time.sleep(0.1)
     Path(fn).unlink()
     d = {
         'node'      : socket.gethostname(),
@@ -59,6 +64,7 @@ def tune_hsaco(task_config, kname, hsaco_id):
 
 @app.task
 def tune_subkernel(task_config, kname):
+    print(f"tune_subkernel {task_config=}")
     nhsaco = probe_nhsaco(kname)
     res = group([tune_hsaco.s(task_config, kname, hsaco_index).set(queue=LOCALQ) for hsaco_index in range(nhsaco)])
     res()
@@ -68,13 +74,14 @@ def do_tune_kernel(task_config):
     sub_kernels = task_config['sub_kernels']
     res = group([tune_subkernel.s(task_config, kname).set(queue=LOCALQ) for kname in sub_kernels])
     res()
+    return task_config
 
 @app.task
 def tune_kernel(task_config):
     # worker_hostname = current_task.request.hostname
     res = chain(preprocess.s(task_config).set(queue=LOCALQ),
-                do_tune_kernel.s(task_config).set(queue=LOCALQ),
-                postprocess.s(task_config).set(queue=LOCALQ))
+                do_tune_kernel.s().set(queue=LOCALQ),
+                postprocess.s().set(queue=LOCALQ))
     res()
 
 @app.task
