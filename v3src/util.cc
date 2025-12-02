f0d6ebcf220559709362e4a83f429f98b14e74b2
Xinya Zhang <Xinya.Zhang@amd.com>
2025-12-02 17:35:40 +0000

    shim: gfx1250 support

--- v3src/util.cc
+++ v3src/util.cc
@@ -59,6 +59,7 @@ std::unordered_map<std::string, GpuClassifier> LazyGpu::string_to_classifier = {
   {"gfx1153", DummyClassifier<GPU_AMD_ARCH_GFX1153_MOD0>() },
   {"gfx1200", DummyClassifier<GPU_AMD_ARCH_GFX1200_MOD0>() },
   {"gfx1201", DummyClassifier<GPU_AMD_ARCH_GFX1201_MOD0>() },
+  {"gfx1250", DummyClassifier<GPU_AMD_ARCH_GFX1250_MOD0>() },
 };
 
 Gpu
@@ -83,8 +84,7 @@ bool isArchExperimentallySupported(hipStream_t stream) {
           vendor_arch == CAT32(GpuVendor::kAMD, 0x1101) ||
           vendor_arch == CAT32(GpuVendor::kAMD, 0x1102) ||
           vendor_arch == CAT32(GpuVendor::kAMD, 0x1103) ||
-          vendor_arch == CAT32(GpuVendor::kAMD, 0x1200) ||
-          vendor_arch == CAT32(GpuVendor::kAMD, 0x1250));
+          vendor_arch == CAT32(GpuVendor::kAMD, 0x1200));
 }
 
 int getMultiProcessorCount(hipStream_t stream) {
