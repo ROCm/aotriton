a579a2f630213f474d6e64a9f01dbe030497cb27
Xinya Zhang <Xinya.Zhang@amd.com>
2026-03-26 12:15:35 -0500

    v3python/tune/flash: renamed flash.py to module.py, reflecting the purpose

--- v3python/tune/flash/__init__.py
+++ v3python/tune/flash/__init__.py
@@ -1,7 +1,7 @@
 # Copyright © 2025 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: MIT
 
-from .flash import (
+from .module import (
     Flash as TuneDesc,
     FlashKernelSelector as ImplSelector,
 )
