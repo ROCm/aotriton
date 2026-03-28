372188c205aaacaa23bd336e8b841dc072306873
Xinya Zhang <Xinya.Zhang@amd.com>
2026-03-28 23:11:30 +0000

    .ci/build-test.sh: support using wheels

--- .ci/build-test.sh
+++ .ci/build-test.sh
@@ -5,15 +5,22 @@ if [ -z "$BASH_VERSION" ]; then
   exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
-  echo 'Missing arguments. Usage: build-test.sh <target arch>' >&2
+if [ "$#" -lt 1 ]; then
+  echo 'Missing arguments. Usage: build-test.sh <target arch> [optional pre-compiled triton wheel]' >&2
   exit 1
 fi
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 . "${SCRIPT_DIR}/common-build.sh"
 
-common_build "$1" "test" \
-  -DAOTRITON_GPU_BUILD_TIMEOUT=0 \
-  -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" \
-  -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold"
+target_arch="$1"
+shift
+
+build_args=("${target_arch}" "test" -DAOTRITON_GPU_BUILD_TIMEOUT=0 -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=mold")
+
+if [ "$#" -ge 1 ]; then
+  wheel=$(realpath "$1")
+  build_args+=("-DAOTRITON_USE_LOCAL_TRITON_WHEEL=${wheel}")
+fi
+
+common_build "${build_args[@]}"
