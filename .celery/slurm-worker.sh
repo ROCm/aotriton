9f0bc1d0a48ddc59357a749eb7b1132c4a378669
Xinya Zhang <Xinya.Zhang@amd.com>
2026-04-11 02:33:42 -0500

    .celery/slurm-worker: remove conflicting --gpus-per-node=8 in script.

--- .celery/slurm-worker.sh
+++ .celery/slurm-worker.sh
@@ -14,7 +14,6 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=8
 #SBATCH --output=logs/slurm-%j.out
 #SBATCH --error=logs/slurm-%j.err
 # Send SIGTERM 30 minutes (1800 seconds) before SLURM timeout
